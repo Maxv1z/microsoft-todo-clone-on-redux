f9021eedf9f52eaa95f2b815607b816e8f9589d8
Maxviz <meitstudying@gmail.com>
2024-01-22 13:08:13 +0200

    removing todo from active todo if clicked again

--- src/features/activeTodo/activeTodoSlice.js
+++ src/features/activeTodo/activeTodoSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createSelector } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { activeTodoId: null };
 
@@ -7,17 +7,16 @@ const activeTodoSlice = createSlice({
     initialState,
     reducers: {
         changeActiveTodo: (state, action) => {
-            return { ...state, activeTodoId: action.payload }
+            if (state.activeTodoId === action.payload && state.activeTodoId !== null) {
+                return { ...state, activeTodoId: null };
+            } else {
+                return { ...state, activeTodoId: action.payload };
+            }
         },
     },
 });
 
-
 export const { changeActiveTodo } = activeTodoSlice.actions;
 export default activeTodoSlice.reducer;
 
-
 export const selectActiveTodo = (state) => state.activeTodo.activeTodoId;
-
-
-
